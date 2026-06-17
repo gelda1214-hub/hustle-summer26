9b73f527475bb4282c449eeaf2d1c18055093e7f
Dagim Gedle <Gelda1214@gmail.com>
2026-06-17 10:34:36 -0700

    Add Python profile variables lab

---
+++ profile.py
@@ -0,0 +1,15 @@
+full_name = "Dagim Gedle"
+
+nickname = "Dagi"
+
+age = 14
+used_python_before = False
+
+hobbies = ["basketball", "video games", "coding"]
+favorites = {
+    "movie": "Home Alone",
+    "food": "Pizza",
+    "hobby": "basketball"
+}
+print(full_name, age, hobbies) 
+
