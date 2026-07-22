7af1a8c931d8f79c40258bf394ddc776e6429f93
Dagim Gedle <Gelda1214@gmail.com>
2026-07-22 10:48:21 -0700

    Laab7 ordering app

---
+++ lab6_dagim/weapon.py
@@ -0,0 +1,7 @@
+class Weapon(Ability):
+  def attack(self):
+    """  This method returns a random value
+    between one half to the full attack power of the weapon.
+    """
+    # TODO: Use integer division to find half of the max_damage value
+    # then return a random integer between half of max_damage and max_damage
